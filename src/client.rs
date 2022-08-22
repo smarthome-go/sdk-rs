4470ab2e30d578b5d689ea30085eea6182b23a61
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-08-22 22:57:42 +0200

    Add credential validation on client creation

--- src/client.rs
+++ src/client.rs
@@ -1,4 +1,7 @@
+use reqwest::{Method, StatusCode};
+
 use crate::{
+    auth::Token,
     errors::{Error, Result},
     version::{check_version, VersionResponse},
     Auth, HTTP_USER_AGENT,
@@ -13,14 +16,14 @@ pub struct Client {
 
 impl Client {
     pub async fn new(raw_url: &str, auth: Auth) -> Result<Self> {
+        // Parse the source url
+        let smarthome_url = reqwest::Url::parse(&raw_url)?;
         // Default client with user agent is created
         let client = reqwest::Client::builder()
             .user_agent(HTTP_USER_AGENT)
             .build()?;
 
-        let smarthome_url = reqwest::Url::parse(&raw_url)?;
-
-        // Fetch the version from the Smarthome server
+        // Fetch the current version from the Smarthome server
         let mut version_url = smarthome_url.clone();
         version_url.set_path("/api/version");
         let res = reqwest::get(version_url).await?;
@@ -32,15 +35,52 @@ impl Client {
         let version = res.json::<VersionResponse>().await?;
 
         // Check if the SDK's version is compatible with the server's version
-        match check_version(&version.version) {
-            Ok(true) => Ok(Self {
+        let client = match check_version(&version.version) {
+            Ok(true) => Self {
                 client,
                 auth,
                 smarthome_url,
                 smarthome_version: version,
-            }),
-            Ok(false) => Err(Error::IncompatibleVersion(version.version)),
-            Err(err) => Err(err),
+            },
+            Ok(false) => return Err(Error::IncompatibleVersion(version.version)),
+            Err(err) => return Err(err),
+        };
+
+        // Attempt to login using the specified credentials
+        match &client.auth {
+            Auth::None => Ok(client),
+            auth => match validate_credentials(&client.smarthome_url, &auth).await {
+                Ok(_) => Ok(client),
+                Err(err) => Err(err),
+            },
         }
+    }
+}
+
+async fn validate_credentials(base_url: &url::Url, auth: &Auth) -> Result<()> {
+    let mut login_url = base_url.clone();
+    // Choose an adequate URL depending on the authentication mode
+    login_url.set_path(match auth {
+        Auth::QueryToken(_) => "/api/login/token",
+        Auth::QueryPassword(_) => "/api/login",
+        _ => unreachable!("login may not be called when using auth method `None`"),
+    });
+
+    // Perform the request
+    let req = reqwest::Client::new().request(Method::POST, login_url);
+    let res = match auth {
+        Auth::QueryPassword(user) => req.json(&user),
+        Auth::QueryToken(token) => req.json(&Token {
+            token: token.to_string(),
+        }),
+        _ => unreachable!("login may not be called when using auth method `None`"),
+    }
+    .send()
+    .await?;
+
+    // Handle smarthome-errors which could occur during login
+    match res.status() {
+        StatusCode::OK | StatusCode::NO_CONTENT => Ok(()),
+        status => Err(Error::Smarthome(status)),
     }
 }
