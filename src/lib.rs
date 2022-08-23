befbf540d8ef3aa68e9d2de51973ba404495c9cb
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-08-23 22:01:34 +0200

    Make crate errors public

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,7 @@ pub use client::Client;
 pub use homescript::exec::*;
 pub use homescript::*;
 pub use power::*;
+pub use errors::*;
 
 const SERVER_VERSION_REQUIREMENT: &str = "^0.2.0";
 const HTTP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
