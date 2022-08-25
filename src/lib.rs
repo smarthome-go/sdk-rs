a90a91c638e43d93809a3de094a6db06e2bb1908
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-08-25 15:30:47 +0200

    Make server version requirement public

--- src/lib.rs
+++ src/lib.rs
@@ -16,5 +16,5 @@ pub use homescript::*;
 pub use power::*;
 pub use errors::*;
 
-const SERVER_VERSION_REQUIREMENT: &str = "^0.2.0";
+pub const SERVER_VERSION_REQUIREMENT: &str = "^0.2.0";
 const HTTP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
