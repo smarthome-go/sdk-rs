dfa64e615aa1e1a7a3922995c2421b5666119fed
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-09-02 13:20:49 +0200

    Add doc comment to version requirement

--- src/lib.rs
+++ src/lib.rs
@@ -11,10 +11,11 @@ mod power;
 pub use auth::{Auth, User};
 pub use client::Client;
 
+pub use errors::*;
 pub use homescript::*;
 pub use homescript::*;
 pub use power::*;
-pub use errors::*;
 
+/// This specifies the version constraints which are validated on a client's creation
 pub const SERVER_VERSION_REQUIREMENT: &str = "^0.2.0";
 const HTTP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
