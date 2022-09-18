663308c5af2885a6707f20ca8257ef934a78b6ac
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-09-18 15:29:26 +0200

    Add system switches getter

--- src/power.rs
+++ src/power.rs
@@ -13,7 +13,7 @@ struct PowerRequest<'request> {
 
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct Switch {
+pub struct PowerSwitch {
     pub id: String,
     pub name: String,
     pub room_id: String,
@@ -36,8 +36,6 @@ pub struct PowerDrawData {
     pub watts: usize,
     pub percent: f64,
 }
-
-
 
 impl Client {
     /// Sets the power state of the given switch to the given value
@@ -81,7 +79,7 @@ impl Client {
     ///     let res = client.personal_switches().await.unwrap();
     /// }
     /// ```
-    pub async fn personal_switches(&self) -> Result<Vec<Switch>> {
+    pub async fn personal_switches(&self) -> Result<Vec<PowerSwitch>> {
         let response = self
             .client
             .execute(self.build_request::<PowerRequest>(
@@ -91,7 +89,33 @@ impl Client {
             )?)
             .await?;
         match response.status() {
-            StatusCode::OK => Ok(response.json::<Vec<Switch>>().await?),
+            StatusCode::OK => Ok(response.json::<Vec<PowerSwitch>>().await?),
+            status => Err(Error::Smarthome(status)),
+        }
+    }
+
+    /// Returns all switches of the target system
+    /// ```rust no_run
+    /// use smarthome_sdk_rs::{Client, Auth};
+    ///
+    /// #[tokio::main]
+    /// async fn main() {
+    ///     let client = Client::new("foo", Auth::None).await.unwrap();
+    ///
+    ///     let res = client.all_switches().await.unwrap();
+    /// }
+    /// ```
+    pub async fn all_switches(&self) -> Result<Vec<PowerSwitch>> {
+        let response = self
+            .client
+            .execute(self.build_request::<PowerRequest>(
+                Method::GET,
+                "/api/switch/list/all",
+                None,
+            )?)
+            .await?;
+        match response.status() {
+            StatusCode::OK => Ok(response.json::<Vec<PowerSwitch>>().await?),
             status => Err(Error::Smarthome(status)),
         }
     }
