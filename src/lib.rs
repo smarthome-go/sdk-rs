f9b7206a82c62d76727654daedbb0cffb7479165
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-10-28 22:39:50 +0200

    Set minimum server version to 0.4

--- src/lib.rs
+++ src/lib.rs
@@ -23,5 +23,5 @@ pub use room::*;
 pub use debug::*;
 
 /// This specifies the version constraints which are validated on a client's creation
-pub const SERVER_VERSION_REQUIREMENT: &str = ">=0.2.0";
+pub const SERVER_VERSION_REQUIREMENT: &str = ">=0.4.0";
 const HTTP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
