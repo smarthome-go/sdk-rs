37e9e7cb3f1514d84770333e2c2f76a5b8e2f843
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2023-05-17 14:49:22 +0200

    feat: Update configuration export

--- src/export.rs
+++ src/export.rs
@@ -1,9 +1,17 @@
 use reqwest::{Method, StatusCode};
+use serde::Serialize;
 
 use crate::{
     errors::{Error, Result},
     Client,
 };
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ExportRequest {
+    pub include_profile_pictures: bool,
+    pub include_cache_data: bool,
+}
 
 impl Client {
     /// Fetches an `export.json` file from the Smarthome server
@@ -14,16 +22,19 @@ impl Client {
     /// async fn main() {
     ///     let client = Client::new(foo", Auth::None).await.unwrap();
     ///
-    ///     let res = client.export_config().await.unwrap();
+    ///     let res = client.export_config(ExportRequest {
+    //          include_profile_pictures: false,
+    //          include_cache_data: false,
+    ///     }).await.unwrap();
     /// }
     /// ```
-    pub async fn export_config(&self) -> Result<String> {
+    pub async fn export_config(&self, request: &ExportRequest) -> Result<String> {
         let response = self
             .client
-            .execute(self.build_request::<()>(
-                Method::GET,
+            .execute(self.build_request::<&ExportRequest>(
+                Method::POST,
                 "/api/system/config/export",
-                None,
+                Some(request),
             )?)
             .await?;
         match response.status() {
